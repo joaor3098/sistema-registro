c320db4101dac95910899b6750c439ba69b8c921
João Raphael <joaoraphael10@hotmail.com>
2022-02-09 16:38:12 -0300

    sistema cadastro

---
+++ cadastro.js
@@ -0,0 +1,37 @@
+let evento = 12
+
+if(evento > 9){
+    console.log("evento permitido, é posterior ao dia 9 !!!")
+} else{
+    console.log("evento não permitido ")
+}
+
+let eventosnúmero = ["Evento Único"];
+console.log(eventosnúmero);
+
+let idade = 20
+
+if(idade > 18){
+    console.log("permitido, participante maior de 18 anos ")
+} else{
+    console.log("não permitido !!!!")
+}
+
+let Quantidade = 9
+
+if(Quantidade < 100){
+    console.log("cadastro permitido, dentro do limite de participantes")
+} else{
+    console.log("Cadastro não permitido, supera o limite de participantes")
+}
+
+let ListaParticicpates = ["Rosangela", "João","Raquel", "Kauã", "Alexandra","Maria"];
+console.log(ListaParticicpates);
+
+let ListaPalestrantes = ["Tiago","Sandy","Marcos"];
+console.log(ListaPalestrantes);
+
+
+
+
+
